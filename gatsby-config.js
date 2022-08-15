8cc497cf2da78e31a71a68403a0ab87550e44f1d
Khalid Belghiti <33843956+KhalidScrypt@users.noreply.github.com>
2022-08-15 12:59:26 +0200

    Change analytics to gtag plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,22 +16,32 @@ module.exports = {
     },
     plugins: [
         'gatsby-plugin-sass',
+        // {
+        //     resolve: 'gatsby-plugin-google-analytics',
+        //     options: {
+        //         trackingId: process.env.GA_TRACKING_ID || '',
+        //         // Puts tracking script in the head instead of the body
+        //         head: true,
+        //         // IP anonymization for GDPR compliance
+        //         anonymize: true,
+        //         // Disable analytics for users with `Do Not Track` enabled
+        //         respectDNT: true,
+        //         // Avoids sending pageview hits from custom paths
+        //         exclude: ['/preview/**'],
+        //         // Specifies what percentage of users should be tracked
+        //         sampleRate: 100,
+        //         // Determines how often site speed tracking beacons will be sent
+        //         siteSpeedSampleRate: 10,
+        //     },
+        // },
         {
-            resolve: 'gatsby-plugin-google-analytics',
+            resolve: `gatsby-plugin-google-gtag`,
             options: {
-                trackingId: process.env.GA_TRACKING_ID || '',
-                // Puts tracking script in the head instead of the body
-                head: true,
-                // IP anonymization for GDPR compliance
-                anonymize: true,
-                // Disable analytics for users with `Do Not Track` enabled
-                respectDNT: true,
-                // Avoids sending pageview hits from custom paths
-                exclude: ['/preview/**'],
-                // Specifies what percentage of users should be tracked
-                sampleRate: 100,
-                // Determines how often site speed tracking beacons will be sent
-                siteSpeedSampleRate: 10,
+                trackingIds: [process.env.GA_TRACKING_ID || ''],
+                pluginConfig: {
+                    head: true,
+                    anonymize_ip: true,
+                },
             },
         },
         'gatsby-plugin-react-helmet',
